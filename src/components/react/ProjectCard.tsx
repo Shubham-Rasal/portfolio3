5fb7a228f50acb4fe3203302e9ef12cceb443734
Shubham-Rasal <bluequbits@gmail.com>
2024-10-12 17:50:58 +0530

    Refactor ProjectCard component to accept props and dynamically render project details; add projects.json for project data; update .gitignore to include Vercel directory.

--- src/components/react/ProjectCard.tsx
+++ src/components/react/ProjectCard.tsx
@@ -1,10 +1,25 @@
-export default function ProjectCard() {
+type Project = {
+  index: number;
+  title: string;
+  description: string;
+  github: string;
+  live?: string;
+  active: boolean;
+};
+
+export default function ProjectCard({
+  title,
+  description,
+  live,
+  github,
+  active,
+}: Project) {
   return (
     <div className="min-w-[120px] flex-col px-4 py-2 border border-gray-200 dark:border-gray-700/70 rounded-lg hover:bg-gray-100 dark:bg-gray-800/70 dark:hover:bg-gray-800 dark:hover:border-gray-700 dark:shadow-[0_0_8px_rgba(0,0,0,0.8)] shadow-[0_0_8px_rgba(0,0,0,0.06)] flex relative ">
       <a
         target="_blank"
-        title="Maazi"
-        href="https://apps.apple.com/us/app/maazi-personal-diary/id1535320472"
+        title={live ? "Live" : github ? "Github" : "Coming soon!"}
+        href={live ? live : github ? github : "#"}
       >
         <div className="absolute top-[10%] right-[10%] z-10 opacity-50 hover:opacity-100 hover:scale-[1.1] duration-100">
           <svg
@@ -46,7 +61,7 @@ export default function ProjectCard() {
           </svg>
         </div>
       </a>
-      <a href="/projects/maazi">
+      <a href={live ? live : github ? github : "#"} target="_blank">
         {/* <div className="py-3 relative">
           <img
             className="w-[40px] drop-shadow-xl"
@@ -56,13 +71,15 @@ export default function ProjectCard() {
         </div> */}
         <div className="pb-1">
           <div className="flex items-center gap-[6px] mt-1">
-            <h3 className="font-medium text-base">Maazi</h3>
+            <h3 className="font-medium text-base">{title}</h3>
             <div
-              className="w-[8px] h-[8px] rounded-full bg-green-500"
-              title="Active"
+              className={`w-[8px] h-[8px] rounded-full  ${
+                active ? "bg-green-500 animate-pulse" : "bg-yellow-500"
+              }`}
+              title={active ? "Active" : "Inactive"}
             ></div>
           </div>
-          <p className="text-sm opacity-80">Journaling app for iOS and Web</p>
+          <p className="text-sm opacity-80 ">{description}</p>
         </div>
       </a>
     </div>
